aab3a3cb6a1cf02d2a2ff8252fdb804a6106f102
Daniel Murray <daniel@nonit.me>
2016-04-22 17:02:20 +0100

    Added code

---
+++ src/io/loyloy/loyhelper/sponge/LoyHelper.java
@@ -0,0 +1,15 @@
+package io.loyloy.loyhelper.sponge;
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "loyhelper", name = "LoyHelper", version = "1.0")
+public class LoyHelper
+{
+    @Listener
+    public void onServerStart( GameStartedServerEvent event )
+    {
+        
+    }
+}
